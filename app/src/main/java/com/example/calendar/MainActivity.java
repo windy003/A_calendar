89634802aa13fd6b6c2124af29b873192b31755a
windy003 <928920471@qq.com>
2025-02-16 15:32:18 +0800

    只有公历ok

---
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private TextView selectedDate;
+    private Button addEventButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+        addEventButton = findViewById(R.id.addEventButton);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+
+            }
+        });
+
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 这里实现添加事件的逻辑
+                Toast.makeText(MainActivity.this, "添加事件", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+} 
